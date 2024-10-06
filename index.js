6b88b966a1eeacf09c59ca67789307e81db615e5
Your Name <youremail@domain.com>
2024-10-06 03:48:49 +0300

    change text using Dom

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", () => {
+    const paragraph = document.getElementById('text');
+    // Update the text
+    paragraph.textContent = 'This is really cool!'; 
+});
